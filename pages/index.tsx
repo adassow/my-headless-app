20103fd84c99319b17b74e38f7042bda1485d025
Adam Sowiński <adassow@gmail.com>
2020-12-08 10:37:04 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 const Home = () => (
-  <h1>My First Deployed App on WP Engine's Headless Platform hook test3!</h1>
+  <h1>My First Deployed App on WP Engine's Headless Platform hook test4!</h1>
 );
 
 export default Home;
